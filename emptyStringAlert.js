19998defb6365cd9072298b1ebae905e3d853bf5
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-12 22:35:20 +0600

    adding new js file emptyStringAlert

---
+++ emptyStringAlert.js
@@ -0,0 +1,9 @@
+function isEmptyString(someString) {
+  if (someString == "") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isEmptyString(""));
+console.log(isEmptyString("The GodFather"));
