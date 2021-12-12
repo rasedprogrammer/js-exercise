75299837e152129764153d39cdbabbe3c84a62f7
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-12 22:28:06 +0600

    writing new method to solving movieRating

--- movieRating.js
+++ movieRating.js
@@ -5,4 +5,16 @@ function giveMovieRating(movieRating) {
     return console.log("Noo, It's A Bad One!");
   }
 }
-console.log(giveMovieRating(8));
+// console.log(giveMovieRating(8));
+
+function giveMovieRating2(movieRating2) {
+  let movieResult;
+  if (movieRating2 >= 7) {
+    movieResult = "Oh It's A Good Movie";
+  } else {
+    movieResult = "Noo, It's A Bad One!";
+  }
+  return movieResult;
+}
+
+console.log(giveMovieRating2(8));
