93d9710cb6134d44b27ccbf19ecd10cc75e26080
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-12 22:07:02 +0600

    adding new Js file getArrayFirstItem Function

---
+++ getArrayFirstItem.js
@@ -0,0 +1,6 @@
+let items = ["banana", "apple", "graps", "painapple"];
+function getFirstArrayItem(items) {
+  return items;
+}
+const firstItem = getFirstArrayItem(items[0]);
+console.log(firstItem);
