c55217ff3dc3f1217f3dff4f0de4fdb76bc8c24d
Md. Rased Molla <75846499+rasedprogrammer@users.noreply.github.com>
2021-12-13 14:15:15 +0600

    adding new problem solving Max Number in numberMaxAndMin Js File

--- numberMaxAndMin.js
+++ numberMaxAndMin.js
@@ -10,3 +10,16 @@ function findMin(numbers) {
 
 console.log(findMin([4,5,6,2,8,9,3]));
 console.log(findMin([4,5,6,8,9,3]));
+
+function findMax(numbers2) {
+  let minimum2 = numbers2[0];
+  for (let i = 0; i < numbers2.length; i++) {
+    if (numbers2[i] > minimum2) {
+      minimum2 = numbers2[i]
+    }
+  }
+  return minimum2;
+}
+
+console.log(findMax([4,5,6,2,8,9,3]));
+console.log(findMax([4,5,6,2,8,3]));
